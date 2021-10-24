ad7130379dd6b7a2016f4aa41afd3a5246d6d6b3
tygrski <77369211+tygrski@users.noreply.github.com>
2021-10-24 10:40:41 -0500

    display note route

--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,41 @@
+//to use app, in terminal run npm install express, npm install fs, npm install path, npm install, npm init.
+// then in terminal run node server.js, and node server.js  each time you run the app after the prior installs
+// open http://localhost:3001/notes in browser
+
 const express = require('express');
-const apiRoutes = require('/routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes')
-const app = express();
+const path = require("path");
+const fs = require("fs");
+let app = express();
+let notes = require("./db/db.json");
 
 const PORT = process.envPORT || 3001; 
 
 // tells if initializing express, we can with json
 app.use(express.json());
-// should be ale to read url coming in, extended:  part enables us to be able to do queries
+// should be able to read url coming in, extended:  part enables us to be able to do queries
 app.use(express.urlencoded({extended: true}));
+
 // use contents of spublic folder
 app.use(express.static('public'));
-app.use('/', htmlRoutes);
-app.use('/api', apiRoutes);
 
+// Routes
+app.get("/notes", function (req, res){
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+});
 
+// Display Notes
+app.get("/api/notes", function(req, res){
+  fs.readFile("db/db.json", "utf8", function(err, data){
+    if(err){
+      console.log(err);
+      return;
+    }
+    res.json(notes);
+  })
+});
+
+// start server
 app.listen(PORT, () => {
   console.log (`App istening at PORT ${PORT}`);
 });
+
